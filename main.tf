d32f3431fbd27ecffca5bc63743df3d468c6839c
jitjoseph <jitjoseph@paypal.com>
2023-06-17 14:07:04 +0530

    adding main.tf change gha-module vpc-id 16

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "web" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  subnet_id              = subnet-018603bb07e03fb6e
+  subnet_id              = "subnet-018603bb07e03fb6e"
   user_data              = <<-EOF
               #!/bin/bash
               echo "Hello, World, its me jithin using github actions" > index.html
