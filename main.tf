fb9c46dec6b903c0148d7259d3e89caa5c05f684
jitjoseph <jitjoseph@paypal.com>
2023-06-17 14:03:14 +0530

    adding main.tf change gha-module vpc-id 14

--- main.tf
+++ main.tf
@@ -32,8 +32,8 @@ resource "aws_instance" "web" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  subnet_id = subnet-018603bb07e03fb6e
-  user_data = <<-EOF
+  subnet_id              = subnet-018603bb07e03fb6e
+  user_data              = <<-EOF
               #!/bin/bash
               echo "Hello, World, its me jithin using github actions" > index.html
               nohup busybox httpd -f -p 8080 &
